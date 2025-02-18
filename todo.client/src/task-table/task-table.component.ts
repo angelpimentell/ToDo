5bf0aede5b6b83a8ed9941ac9a2a030d5b5aecf6
Angel Pimentel <angelpimentelcontact@gmail.com>
2025-02-18 16:00:09 -0400

    Add task table

---
+++ todo.client/src/task-table/task-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-table',
+  standalone: false,
+  templateUrl: './task-table.component.html',
+  styleUrl: './task-table.component.css'
+})
+export class TaskTableComponent {
+
+}
