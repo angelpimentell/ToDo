5b3b217809712767ef25d05f36d5ca1364a96eb7
Angel Pimentel <angelpimentelcontact@gmail.com>
2025-02-19 11:54:26 -0400

    Change app routing

--- todo.client/src/app/app-routing.module.ts
+++ todo.client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TaskTableComponent } from './task-table/task-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'tasks', component: TaskTableComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
